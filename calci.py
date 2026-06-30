adebff8c74da656da0f73de11cfcf8ad1a28c379
pkavya9 <245123737097@mvsrec.edu.in>
2026-06-30 11:00:26 +0530

    Update calci.py

--- calci.py
+++ calci.py
@@ -1,7 +1,8 @@
-a = 10
-b = 5
-
-print(f"ADD = {a} + {b} = {a + b}")
-print(f"SUB = {a} - {b} = {a - b}")
-print(f"MUL = {a} * {b} = {a * b}")
-print(f"DIV = {a} / {b} = {a / b}")
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mul(a, b): return a * b
+def div(a, b): return a / b if b != 0 else 'Error'
+print('ADD(10,5):', add(10,5))
+print('SUB(10,5):', sub(10,5))
+print('MUL(10,5):', mul(10,5))
+print('DIV(10,5):', div(10,5))
